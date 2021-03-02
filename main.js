a7615f3772118e179a7838f42122b734f532a08d
annpermi <perminova.anna85@gmail.com>
2021-03-02 16:38:41 -0500

    added main.js file

---
+++ main.js
@@ -0,0 +1,7 @@
+var numberOfChildren = 2;
+var partner = 'Denis';
+var geoLocation = 'Italy';
+var jobTitle = 'Developer';
+
+var future ='You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' + partner + ' with ' + numberOfChildren + ' kids.';
+console.log(future);
